3f43a9a1b1f1c3468b375e56a5b28aa2c33eb857
boyoung <qhdudtls97@gmail.com>
2022-11-07 21:41:18 +0900

    누락파일 업뎃

---
+++ RPDataConverter/RPDataConverter/Result.cpp
@@ -0,0 +1,58 @@
+﻿// Result.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "RPDataConverter.h"
+#include "Result.h"
+#include "afxdialogex.h"
+
+
+// CResult 대화 상자
+
+IMPLEMENT_DYNAMIC(CResult, CDialogEx)
+
+CResult::CResult(CMapFile *pFile, CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG_RESULT, pParent)
+{
+	m_pCMapFile = pFile;
+}
+
+CResult::~CResult()
+{
+}
+
+void CResult::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST_RESULT, m_listctlResult);
+}
+
+
+BEGIN_MESSAGE_MAP(CResult, CDialogEx)
+END_MESSAGE_MAP()
+
+BOOL CResult::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+	PrintResult();
+
+
+
+	return TRUE;
+}
+
+// CResult 메시지 처리기
+BOOL CResult::PrintResult()
+{
+	std::vector<CString> vGridID;
+	vGridID = m_pCMapFile->getGridIdList();
+	CString str;
+
+	for (int i = 0; i < vGridID.size(); i++)
+	{
+		//str.Format(_T("%s"), vGridID[i]);
+		m_listctlResult.InsertItem(0, vGridID[i]);
+	}
+
+	return TRUE;
+}
---
+++ RPDataConverter/RPDataConverter/Result.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "MapData.h"
+
+// CResult 대화 상자
+
+class CResult : public CDialogEx
+{
+	DECLARE_DYNAMIC(CResult)
+
+public:
+	CResult(CMapFile *pFile, CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CResult();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_RESULT };
+#endif
+
+protected:
+	virtual BOOL OnInitDialog();
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	BOOL PrintResult();
+
+private:
+	CMapFile* m_pCMapFile;
+	CListCtrl m_listctlResult;
+};
